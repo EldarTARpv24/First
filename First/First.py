129a192d5e250e4339a1a306fd7d25f1139f078d
Deniel Kruusman <denielkruusman@gmail.com>
2025-01-20 11:25:46 +0200

    Add project files.

---
+++ First/First.py
@@ -0,0 +1,28 @@
+print("Tere-tere")
+
+#типы данных - int(целочисленные), float(с плавающей точкой), str(слово), bool \
+
+#a = 5
+#aaste = 2
+#ans = a**aaste
+#print(a,"**", aaste, "=", ans)
+
+# first = int(input("Введите первый множитель"))
+# second = int(input("Введите второй множитель"))
+# answer = first * second
+# print(answer)
+
+# first = int(input("Первое: "))
+# second = int(input("Второе: "))
+# answ = first / second
+# print(answ)
+
+# first = int(input("Первое: "))
+# second = int(input("Второе: "))
+# answ = first % second
+# print(answ)
+
+first = int(input("Первое: "))
+second = int(input("Второе: "))
+answ = first // second
+print(answ)
