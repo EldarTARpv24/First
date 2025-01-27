78f2d9c2b3ae19c445e30edef1bda9fc2871c2c0
EldarA <eldar040503@gmail.com>
2025-01-27 10:52:55 +0200

    s

--- First/module228.py
+++ First/module228.py
@@ -1,3 +1,20 @@
 ﻿#upper() - делает всё верхним регистром
 #lower() - делает все нижним регистром
 #capitalize() - делает первую букву в верхний регистр
+
+# print(3 + 8 / (4 - 2) * 4)
+#19
+
+# print(3 + 8 / 4 - 2 * 4)
+#-3
+
+from math import *
+try:
+    r = float(input("Введите радиус: "))
+    scube = round((r * 2) ** 2, 2)
+    so = round(pi * r ** 2, 2)
+    pcube = round(r * 2 * 4, 2)
+    po = round(2 * pi * r,2)
+except:
+    print("Только цифры")
+print(f"Площади равны {scube} и {so}, Периметры равны {pcube} и {po}")
